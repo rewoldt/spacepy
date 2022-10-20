8a1046ad6ad3062e11af98686357af35ae643ed3
Amy Rewoldt <rewoldt@csrlap2022-0130.attlocal.net>
2022-10-20 16:21:50 -0400

    Update to pybats with file reader script reconx.py

---
+++ spacepy/pybats/reconx.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+'''
+Some simple tools to handle RECONX output.
+'''
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_separator(filename):
+    '''Read a separator file and return a dictionary-like data object.'''
+
+    with open(filename, 'r') as f:
+        # Read one line, parse header:
+        line = f.readline()
+        head = re.findall('\"(.+?)\s\[(.+?)\]\"', line)
+
+        # Extract variable names and units.
+        var, unit = [], []
+        for pair in head:
+            var.append(pair[0])
+            unit.append(pair[1])
+
+        # Skip next line in header:
+        f.readline()
+
+        # Read remainder of lines:
+        lines = f.readlines()
+
+    # Create container for data:
+    data = {}
+    for v in var:
+        data[v] = np.zeros(len(lines))
+
+    # Put data into the container
+    for i, l in enumerate(lines):
+        parts = l.split()
+        for v, p in zip(var, parts):
+            data[v][i] = p
+
+    return data
+
