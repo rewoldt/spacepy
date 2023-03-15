727a65b811ebd3a636863e951b34fe720376ad44
Amy Rewoldt <rewoldt@csrlap2022-0130.local>
2023-03-15 12:49:37 -0400

    change to reconx.py file reader

--- spacepy/pybats/reconx.py
+++ spacepy/pybats/reconx.py
@@ -40,3 +40,35 @@ def read_separator(filename):
 
     return data
 
+def read_nulls(filename):
+    '''Read a null file and return a dictionary-like data object.'''
+
+    with open(filename, 'r') as f:
+        # Read one line, parse header:
+        line = f.readline()
+        head = re.findall('\"(.+?)\"', line)
+
+        # Extract variable names and units.
+        var, unit = [], []
+        for pair in head:
+            var.append(pair[0])
+            #unit.append(pair[1])
+
+        # Skip next line in header:
+        f.readline()
+
+        # Read remainder of lines:
+        lines = f.readlines()
+
+    # Create container for data:
+    data = {}
+    for v in var:
+        data[v] = np.zeros(len(lines))
+
+    # Put data into the container
+    for i, l in enumerate(lines):
+        parts = l.split()
+        for v, p in zip(var, parts):
+            data[v][i] = p
+
+    return data
